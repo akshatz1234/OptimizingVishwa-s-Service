4f7fd0a4efcaa68030afee1b5667a86f38230753
Akshat Zala <akshat.z@arnekt.com>
2020-07-21 18:47:49 +0530

    Optimized code

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
+# from app import app
 import pytesseract
 from flask import Flask, request
 import numpy as np
-from PIL import Image
-from PIL import ImageEnhance
-import cv2 as cv
+from PIL import Image, ImageEnhance
+import cv2
 import re
 import imutils
 import util_aadhar
@@ -13,29 +13,29 @@ import util_vi
 import util_pass
 import util_other
 
-# Preprocess image file
-def preprocess(path):
-    img = cv.imread(path,0)
-    blurred = cv.blur(img, (3,3))
-    canny = cv.Canny(blurred, 50, 200)
-    pts = np.argwhere(canny>0)
-    y1,x1 = pts.min(axis=0)
-    y2,x2 = pts.max(axis=0)
-    cropped = img[y1:y2, x1:x2]
-    imS = imutils.resize(cropped, width=950)
-    cv.imwrite('/home/akshatz/Documents/project/temp1.jpg',imS);
-    image = Image.open('/home/akshatz/Documents/project/temp1.jpg')
-    enhancer = ImageEnhance.Brightness(image)
-    enhanced_im = enhancer.enhance(1.7)
-    con = ImageEnhance.Contrast(enhanced_im)
-    con1 = con.enhance(1.3)
-    enhancer_object = ImageEnhance.Sharpness(con1)
-    out = enhancer_object.enhance(3)
-    out.save("/home/akshatz/Documents/project/t2.jpg")
-    i = cv.imread("/home/akshatz/Documents/project/t2.jpg",0)
-    output = pytesseract.image_to_string(i, lang='eng')
-    return(output)
- 
+# allowed filenames
+ALLOWED_EXTENSIONS = ['png', 'jpg', 'jpeg']
+
+# function to denote allowed file formats
+def allowed_file(file):
+    return ('.' in file and \
+           file.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS)
+
+# main Function
+app = Flask(__name__)
+# @app.route("/card", methods=['GET', 'POST'])
+def rear():
+    f = request.files['file']
+    f.save("/home/akshatz/Documents/project/temp1.jpg")
+    img = Image.open(f)
+    img.load()
+    if allowed_file(f):
+        text = pytesseract.image_to_string(img)
+        return cat(text)
+    else: 
+        text = pytesseract.image_to_string(img)
+        return cat(text)
+
 # categorization
 def cat(out):
     num = re.search("([0-9]{4}\ [0-9]{4}\ [0-9]{4})", out)
@@ -63,31 +63,3 @@ def cat(out):
             return(util_pan.main_ex(out))
     else:
         return(util_aadhar.main_ex(out))
-    
-# input file
-filename = "/home/akshatz/Downloads/ID proofs/Voter ID/Suyash.jpg"
-
-# allowed filenames
-ALLOWED_EXTENSIONS = ['png', 'jpg', 'jpeg']
-
-# function to denote allowed file formats
-def allowed_file(filename):
-    return '.' in filename and \
-           filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
-
-# main Function
-app = Flask(__name__)
-@app.route("/card", methods=['GET', 'POST'])
-def rear():
-    if allowed_file(filename):
-        img = Image.open(filename)
-        img.load()
-        text = pytesseract.image_to_string(img)
-        return (cat(preprocess(filename)))
-        if text == None:
-            text = cat(preprocess(filename))
-            return text 
-
-if __name__ == "__main__":
-    app.debug = True
-    app.run(host = '127.0.0.1',port=5000, threaded = False)
