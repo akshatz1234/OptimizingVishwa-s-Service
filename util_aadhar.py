d9bd4d778b8c401cf24411e89a9ad90c95deacdc
Akshat Zala <akshat.z@arnekt.com>
2020-07-21 14:40:24 +0530

    Running on all the images

--- util_aadhar.py
+++ util_aadhar.py
@@ -1,18 +1,6 @@
 import commonUtility
 import re 
-def genex(tokenized_text):
-    """
-    In: OCR output Tokenized
-    out: Gender
-    """
-    for i in tokenized_text:
-        male = re.search("(^MA.E$|^Ma.e$)", i)
-        female = re.search("(^FEMA.E$|^Fema.e$)", i)
-        if male is not None:
-            return "Male"
-        if female is not None:
-            return "Female"
-        
+       
         
 def main_ex(output):
     tokenized_text = commonUtility.word_tokenize(output)
@@ -22,7 +10,7 @@ def main_ex(output):
     data['dob'] = commonUtility.dateex(output)
     data['age'] = commonUtility.age(data['dob'])
     data['docType'] = "Aadhar_Card"
-    data['gender'] = genex(tokenized_text)
+    data['gender'] = commonUtility.genex(tokenized_text)
     data['bloodGroup'] = ""
     data['address'] = ""
     return commonUtility.jsonify(data)
--- util_other.py
+++ util_other.py
@@ -1,31 +1,14 @@
 import commonUtility
 
-def bloodGroup(output):
-    regex = r"\bB\+|\bB-|\bA\+|\bA-|\bAB\+|\bAB-|\bO\+|\bO-"
-    bg = re.search(regex, output)
-    if bg is None:
-        return("None")
-    else:
-        return(bg.group(0))
-
-def doctype(output):
-    emp = re.search("Employee|EMPLOYEE", output)
-    if emp is None:
-        return("None")
-    else:
-        return("Employee_id")
-    
-        
 def main_ex(output):
-    tokenized_text = word_tokenize(output)
-    classified_text = st.tag(tokenized_text)
-    # print(classified_text)
+    tokenized_text = commonUtility.word_tokenize(output)
+    classified_text = commonUtility.st.tag(tokenized_text)
     data = {}
-    data['name'] = nameex(classified_text)
-    data['dob'] = dateex(output)
-    data['age'] = age(data['dob'])
-    data['docType'] = doctype(output)
-    data['gender'] = genex(tokenized_text)
-    data['bloodGroup'] = bloodGroup(output)
+    data['name'] = commonUtility.nameex(classified_text)
+    data['dob'] = commonUtility.dateex(output)
+    data['age'] = commonUtility.age(data['dob'])
+    data['docType'] = "EmployeeID"
+    data['gender'] = commonUtility.genex(tokenized_text)
+    data['bloodGroup'] = commonUtility.bloodGroup(output)
     data['address'] = ""
-    return jsonify(data)
+    return commonUtility.jsonify(data)
