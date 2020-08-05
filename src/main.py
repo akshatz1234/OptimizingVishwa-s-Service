89c7e6f34085277ab7956595a90c201dc2897f2e
Akshat Zala <akshat.z@arnekt.com>
2020-08-05 19:17:51 +0530

    Removed print statements

--- src/main.py
+++ src/main.py
@@ -14,46 +14,48 @@ import util_other
 
 #preprocess
 def preprocess(path):
-    img = cv.imread(path,0)
-    blurred = cv.blur(img, (5,5))
-    canny = cv.Canny(blurred, 50, 200)
-    pts = np.argwhere(canny>0)#edges of document
-    y1,x1 = pts.min(axis=0)
-    y2,x2 = pts.max(axis=0)
-    cropped = img[y1:y2, x1:x2]# cropping 
-    imS = imutils.resize(cropped, width=950)# resizing
-    cv.imwrite('../project/temp1.jpg',imS)#rewrite image
-    image = Image.open('../project/temp1.jpg')# open image
-    enhancer = ImageEnhance.Brightness(image)#brightness
-    enhanced_im = enhancer.enhance(1.7)#enhance image
-    con = ImageEnhance.Contrast(enhanced_im)#contrast 
-    con1 = con.enhance(1.3)# enhance
-    enhancer_object = ImageEnhance.Sharpness(con1)#sharpness
-    out = enhancer_object.enhance(3)#enhance
-    out.save("../project/t2.jpg")
-    i = cv.imread("../project/t2.jpg",0)
-    text = pytesseract.image_to_string(i, lang='eng')
-    if text == "":
-        text = pytesseract.image_to_string(img, lang='eng')
-        return text
-    else:
+    try:
+        img = cv.imread(path,0)
+        blurred = cv.blur(img, (5,5))
+        canny = cv.Canny(blurred, 50, 200)
+        pts = np.argwhere(canny>0)#edges of document
+        y1,x1 = pts.min(axis=0)
+        y2,x2 = pts.max(axis=0)
+        cropped = img[y1:y2, x1:x2]# cropping 
+        imS = imutils.resize(cropped, width=950)# resizing
+        cv.imwrite('../project/temp1.jpg',imS)#rewrite image
+        image = Image.open('../project/temp1.jpg')# open image
+        enhancer = ImageEnhance.Brightness(image)#brightness
+        enhanced_im = enhancer.enhance(1.7)#enhance image
+        con = ImageEnhance.Contrast(enhanced_im)#contrast 
+        con1 = con.enhance(1.3)# enhance
+        enhancer_object = ImageEnhance.Sharpness(con1)#sharpness
+        out = enhancer_object.enhance(3)#enhance
+        out.save("../project/t2.jpg")
+        i = cv.imread("../project/t2.jpg",0)
         text = pytesseract.image_to_string(i, lang='eng')
-        return(text)
-
+        if text == "":
+            text = pytesseract.image_to_string(img, lang='eng')
+            return text
+        else:
+            text = pytesseract.image_to_string(i, lang='eng')
+            return(text)
+    except:
+        return "Cannot preprocess the file"
 
 # categorization
 def cat(out):
     try:
-        if re.search("([0-9]{4}\ [0-9]{4}\ [0-9]{4})", out):# Aadhaar card
+        if re.search("[0-9]{4}\ [0-9]{4}\ [0-9]{4}", out):# Aadhaar card
             return(util_aadhar.main_ex(out))
-        elif re.search("([A-Z]{5}[0-9]{4}[A-Z]{1})", out):# pan card
+        elif re.search("[A-Z]{5}[0-9]{4}[A-Z]{1}", out):# pan card
             return util_pan.main_ex(out)
         elif re.search("ELECTION", out): # Election ID
             return(util_vi.main_ex(out))
-        elif re.search("[A-Z]{1}[0-9]{6}", out):# passport
-            return(util_pass.main_ex(out))
-        elif re.search("([A-Z]{2}[0-9]{2} [0-9]{11})", out):#driving license
+        elif re.search("[A-Z]{2}[0-9]{2} [0-9]{11}", out):#driving license
             return(util_dl.main_ex(out))
+        elif re.search("\b[A-Z]{1}[0-9]{6}\b", out):# passport
+            return(util_pass.main_ex(out))
         else:
             return(util_other.main_ex(out))
     except:
