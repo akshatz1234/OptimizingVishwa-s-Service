2740e70100e7ea1c54efc0e167cd269d869cdfb1
Akshat Zala <akshat.z@arnekt.com>
2020-08-06 11:26:56 +0530

    Try/ Catch in all the methods of commonUtility

--- src/commonUtility.py
+++ src/commonUtility.py
@@ -16,26 +16,38 @@ def nameex(txt):
     In: Classified text from StanfordNER
     Out: Name
     """
-    for token, tag in groupby(txt, lambda x:x[1]):
-        if token == 'PERSON' and token !="O":
-            return " ".join(str(x) for x, t in tag)
-            break
-
+    try:
+        continuous_chunk = []
+        current_chunk = []
+        for token, tag in txt:
+            if tag == "PERSON": 
+                current_chunk.append(token)
+            else:
+                if current_chunk: 
+                    continuous_chunk.append(current_chunk)
+        if current_chunk:
+            current_chunk = " ".join(str(x) for x in current_chunk)
+            return current_chunk
+    except:
+        return "None"
 # date extraction
 def dateex(output):
     """
     In: Output from the OCR
     out: Date
     """
-    date = re.search("([0-9]{2}\/[0-9]{2}\/[0-9]{4}|[0-9]{2}\-[0-9]{2}\-[0-9]{4})", output)
-    if date is None:
-        year = re.search("(19..|20..)", output)
-        if year is None:
-            return None
+    try:
+        date = re.search("([0-9]{2}\/[0-9]{2}\/[0-9]{4}|[0-9]{2}\-[0-9]{2}\-[0-9]{4})", output)
+        if date is None:
+            year = re.search("(19..|20..)", output)
+            if year is None:
+                return None
+            else:
+                return(year.group(1))
         else:
-            return(year.group(1))
-    else:
-        return(date.group(1))
+            return(date.group(1))
+    except: 
+        return "None"
 
 
 def age(dob):
@@ -52,19 +64,24 @@ def genex(tokenized_text):
     In: OCR output Tokenized
     out: Gender
     """
-    for i in tokenized_text:
-        male = re.search("(^MA.E$|^Ma.e$)", i)
-        female = re.search("(^FEMA.E$|^Fema.e$)", i)
-        if male is not None:
-            return "Male"
-        if female is not None:
-            return "Female"
-
+    try:
+        for i in tokenized_text:
+            male = re.search("(^MA.E$|^Ma.e$)", i)
+            female = re.search("(^FEMA.E$|^Fema.e$)", i)
+            if male is not None:
+                return "Male"
+            if female is not None:
+                return "Female"
+    except:
+        return "None"
 
 def bloodGroup(output):
-    regex = r"\bB\+|\bB-|\bA\+|\bA-|\bAB\+|\bAB-|\bO\+|\bO-"
-    bg = re.search(regex, output)
-    if bg is None:
-        return("None")
-    else:
-        return(bg.group(0))
+    try:
+        regex = r"\bB\+|\bB-|\bA\+|\bA-|\bAB\+|\bAB-|\bO\+|\bO-"
+        bg = re.search(regex, output)
+        if bg is None:
+            return("None")
+        else:
+            return(bg.group(0))
+    except:
+        return "None"
